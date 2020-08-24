ba4d6180b44ca85f76a6e7a01fb1f1510646bb55
Paulo Sa <paulojvdesa@gmail.com>
2020-08-24 17:05:37 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+
 @Service
 public class UserService {
 	@Autowired
