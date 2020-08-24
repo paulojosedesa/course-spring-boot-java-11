ae3c36944984dd5a5536bc2c13f9fa84c5e224f4
Paulo Sa <paulojvdesa@gmail.com>
2020-08-24 17:17:48 -0300

    Exception Handling - delete

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -1,10 +1,14 @@
 package com.educandoweb.course.services;
 import java.util.List;
 import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -29,9 +33,14 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+			} catch (EntityNotFoundException e) {
+				throw new ResourceNotFoundException(id);
+				
+			}
 	}
 
 	private void updateData(User entity, User obj) {
